2d2b4a38606ef3aa5f4db2f93d612eba3c9474df
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-25 20:06:19 -0700

    add a list of items

--- movie-rental/src/components/ListGroup.tsx
+++ movie-rental/src/components/ListGroup.tsx
@@ -1,5 +1,11 @@
 function ListGroup() {
-    return <h1> List Group</h1>
+    return <ul className="list-group">
+    <li className="list-group-item">An item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>
 }
 
 export default ListGroup;
